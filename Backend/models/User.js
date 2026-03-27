842ef7e7f383c99588a8debe2f3f33c8f3c021c3
Pradeepa <153888347+pradeepaanu@users.noreply.github.com>
2026-03-27 12:59:12 +0530

    Add files via upload

---
+++ Backend/models/User.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: [true, 'Please provide a full name'],
+    trim: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: [true, 'Please provide a phone number'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 6,
+    select: false,
+  },
+  companyName: {
+    type: String,
+    trim: true,
+  },
+  isAgency: {
+    type: Boolean,
+    default: false,
+  },
+  profileImage: {
+    type: String,
+    default: null,
+  },
+  bio: {
+    type: String,
+    default: '',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
